aacab58a0de5c0ef10954fdf0347f69c273a9f04
geopras <geopras84@gmail.com>
2016-10-19 16:40:05 +0200

    WebSocket-Klasse erweitert um Eventmethoden

--- Backend/src/de/ideaWatcher/endpoint/WebSocketEndpoint.java
+++ Backend/src/de/ideaWatcher/endpoint/WebSocketEndpoint.java
@@ -1,7 +1,37 @@
 package de.ideaWatcher.endpoint;
 
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+
+@ServerEndpoint("/wsEndpoint")
 public class WebSocketEndpoint {
 
 	// implementation of Task #10 - WebSocketConnection goes here
-	
+    @OnMessage
+    public void getMessage(Session session, String msg) {
+        try {
+            if (session.isOpen()) {
+                session.getBasicRemote().sendText(msg);
+            }
+        } catch (IOException e) {
+            try {
+                session.close();
+            } catch (IOException e1) {
+                // Ignore
+            }
+        }
+    }
+
+    @OnOpen
+    public void open(Session session) {
+    }
+
+    @OnClose
+    public void close(Session session) {
+    }
+
+    @OnError
+    public void onError(Throwable error) {
+    }
 }
