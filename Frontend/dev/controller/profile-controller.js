6cc1b0182b96943c9c6e7127f6aac613cffed4d9
stefanwebo <webo@nurfuerspam.de>
2016-11-07 10:54:34 +0100

    - Navigation für ProfileView hinzugefügt

---
+++ Frontend/dev/controller/profile-controller.js
@@ -0,0 +1,35 @@
+ideaWatcher.controller.Profile = ideaWatcher.controller.Profile || (function CProfile() {
+
+        var cbShowView = null;
+        var evSwitchView = {
+            topic: 'switchView/profile',
+            cbFunction: cbSwitchView
+        };
+
+        //region subscribe to events
+        ideaWatcher.core.MessageBroker.subscribe(evSwitchView);
+        //endregion
+
+        //region Callback: Internal - SwitchView
+        function cbSwitchView(obj)
+        {
+            cbShowView({
+                shouldShow: obj.shouldShow
+            });
+        }
+        //endregion
+
+        //region register Callbacks
+        function pubRegisterShowView(cb) {
+            cbShowView = cb;
+        }
+        //endregion
+
+        // diese Methoden stellen die öffentliche API dar, über welche mit dem Modul kommuniziert werden kann
+        return {
+            // hier kann die View eine Methode(ui-Connector) registrieren, die gerufen wird,
+            // wenn die View ein/ausgeblendet werden soll
+            registerShowView: pubRegisterShowView
+        };
+
+    })();
