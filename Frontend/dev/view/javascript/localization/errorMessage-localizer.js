530ecccdb7d6855dc5f8d0b5efe5ae17884dab7d
Bassbiene <bassbiene@gmail.com>
2016-11-09 11:29:56 +0100

    ErrorMessage-localizer angelegt

--- Frontend/dev/view/javascript/localization/errorMessage-localizer.js
+++ Frontend/dev/view/javascript/localization/errorMessage-localizer.js
@@ -7,19 +7,16 @@ ideaWatcher.core.Localizer.ErrorMessage = {
     	SLogin_getUser_error: "The database query for your login has failed. Please contact the WebApi administrator.",
     	SLogin_password_not_valid: "Login has failed.", 
     	
-    	SSignup_existsUser_error: "The database query for your sign-up has failed. Please contact the WebApi administrator.",
     	SSignup_addUser_error: "An error occuring while adding the user to the database. Please contact the WebApi administrator.",
-    	SSignup_username_already_exists: "You cannot sign-up with this username, because it already exists.",
-    	Websocket_requestHandling_error: "",			
+    	SSignup_existsUser_error: "The database query for your sign-up has failed. Please contact the WebApi administrator.",
+    	SSignup_username_already_exists: "Sign-up has failed because this username already exists.",			
     },
     de_DE: {
     	SLogin_getUser_error: "Die Anmeldung ist bei der Datenbankabfrage des Benutzers fehlgeschlagen. Bitte wenden Sie sich an den WebApi-Administrator.",
     	SLogin_password_not_valid: "Die Anmeldung ist fehlgeschlagen.",
     	
-    	SSignup_existsUser_error: "Die Registrierung ist bei der Datenbankabfrage, ob der Benutzer bereits existiert, fehlgeschlagen. Bitte wenden Sie sich an den WebApi-Administrator.",
     	SSignup_addUser_error: "Die Registrierung ist bei der Datenbankoperation \"Hinzufügen eines neuen Benutzers\" fehlgeschlagen. Bitte wenden Sie sich an den WebApi-Administrator.",
+    	SSignup_existsUser_error: "Die Registrierung ist bei der Datenbankabfrage, ob der Benutzer bereits existiert, fehlgeschlagen. Bitte wenden Sie sich an den WebApi-Administrator.",
     	SSignup_username_already_exists: "Die Registrierung ist nicht möglich, da der Benutzername bereits existiert.",
-    	
-    	Websocket_requestHandling_error: "Beim Behandeln des Requests ist ein Fehler aufgetreten! Die WebSocket-Verbindung wird geschlossen werden!\nFehlermeldung: ",
     }
 }
