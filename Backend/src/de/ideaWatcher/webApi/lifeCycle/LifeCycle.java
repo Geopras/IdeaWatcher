c409982bf27961d2bd73ff8b23fdeb61db84b83d
LoxGITLox <sunrise122@gmx.de>
2016-11-09 12:20:39 +0100

    WebListener Annotation hinzugefügt, damit die beiden Lifecycle Methoden gerufen werden

--- Backend/src/de/ideaWatcher/webApi/lifeCycle/LifeCycle.java
+++ Backend/src/de/ideaWatcher/webApi/lifeCycle/LifeCycle.java
@@ -2,7 +2,9 @@ package de.ideaWatcher.webApi.lifeCycle;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
+@WebListener
 public class LifeCycle implements ServletContextListener{
 
 	// Logic for Container StartUp/Shutdown goes here
@@ -11,11 +13,13 @@ public class LifeCycle implements ServletContextListener{
 	public void contextDestroyed(ServletContextEvent arg0)
 	{
 		//shutdown logic
+		
 	}
 
 	@Override
 	public void contextInitialized(ServletContextEvent arg0)
 	{
 		//startup logic
+		System.out.println("Anwendung gestartet");
 	}
 }
