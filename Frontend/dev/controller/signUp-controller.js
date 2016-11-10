db4f1fb23b7c0d5699f5a42d59319efa393459c0
Bassbiene <bassbiene@gmail.com>
2016-11-10 20:26:22 +0100

    Registrierung prüft ob Passwort lang genug ist
    und ob Passwörter übereinstimmen... Ausgaben erfolgen auf Konsole.

---
+++ Frontend/dev/controller/signUp-controller.js
@@ -0,0 +1,98 @@
+ideaWatcher.controller.Signup = ideaWatcher.controller.Signup || (function () {
+
+    var userName = null;
+    var email = null;
+    var password = null;
+    var cbShowView = null;
+    var evSwitchView = {
+        topic: 'switchView/signup',
+        cbFunction: cbSwitchView
+    };
+    //endregion
+
+    //region subscribe to events
+    ideaWatcher.core.MessageBroker.subscribe(evSwitchView);
+    //endregion
+
+    //region TryToSignup
+    function pubTryToSignup(exObject)
+    {
+        // BN+PW zwischenspeichern, wird von buildRequestLogin() Methode an 2 Stellen Benötigt
+        userName = exObject.userName;
+        email = exObject.email;
+        password = exObject.password;
+
+        // Wenn bereits eine Verbindung zum Backend besteht, wird der Request an das Backend geschickt
+        if (ideaWatcher.core.WebSocketConnector.isConnected()) {
+            ideaWatcher.core.WebSocketConnector.sendRequest(buildRequestLogin());
+        }
+        else {
+            // Versuche eine Verbindung zum Backend aufzubauen
+            ideaWatcher.core.WebSocketConnector.connectToServer(WS_URL, cbConnectionEstablished);
+        }
+    }
+    //endregion
+
+    //region build: RequestSignup
+    function buildRequestSignup()
+    {
+        // das könnte man in das Model auslagern... sinnvoll?
+        var exSignupRequest = ideaWatcher.model.Request;
+        exSignupRequest.destination = 'SSignup/signupRequest';
+        exSignupRequest.data = {
+            userName: userName,
+            email: email,
+            password: password
+        };
+
+        return exLoginRequest;
+    }
+    //endregion
+
+    //region Callback: connection established
+    function cbConnectionEstablished(isConnected,errorObject)
+    {
+        if (isConnected) {
+            // Wenn die Verbindung erfolgreich hergestellt wurde, wird der Request an das Backend geschickt
+            ideaWatcher.core.WebSocketConnector.sendRequest(buildRequestSignup());
+        }
+        else {
+            // Verbindung konnte nicht hergestellt werden, nochmal versuchen?
+            // oder gleich der Error anzeigen? in eigener Komponente?
+            console.log('Verbindungsaufbau gescheitert');
+            console.log(errorObject);
+        }
+    }
+    //endregion
+
+    //region Callback: Internal - SwitchView
+    function cbSwitchView(obj)
+    {
+        // if(obj.shouldShow) wam.logic.Header.showHeader(false);
+        cbShowView({
+            shouldShow: obj.shouldShow
+        });
+    }
+    //endregion
+
+    //region register Callbacks
+    // function pubRegisterVerificationError(cb) {
+    //     cbVerificationError = cb;
+    // }
+    function pubRegisterShowView(cb) {
+        cbShowView = cb;
+    }
+    //endregion
+
+    // diese Methoden stellen die öffentliche API dar, über welche mit dem Modul kommuniziert werden kann
+    return {
+        // hier kann die View eine Methode(ui-Connector) registrieren, die gerufen wird,
+        // wenn die View ein/ausgeblendet werden soll
+        registerShowView: pubRegisterShowView,
+        // stellt die Öffentliche Schnittstelle dar, mit der die View dem Controler sagen kann,
+        // dass der Benutzer den Button gedrückt hat und jetzt die Login-Credential an das Backend
+        //geschickt werden sollen, um den Benutzer zu validieren
+        tryToSignup: pubTryToSignup
+    };
+
+})();
---
+++ Frontend/dev/view/javascript/uiConnector/signup-uiConnector.js
@@ -0,0 +1,114 @@
+ideaWatcher.view.Signup = ideaWatcher.view.Signup || (function VSignup() {
+
+    //region local vars
+    // Event Globale Initialisierung
+    var evIni = {
+        topic: 'internal/ini',
+        cbFunction: cbIni
+    };
+
+    var htmlFormSignup = null;
+    var htmlUsernameInput = null;
+    var htmlEmailInput = null;
+    var htmlPasswordInput = null;
+    var htmlPasswordRepeatInput = null;
+    var htmlSubmitButton = null;
+    var htmlView = null;
+    var textSignup = {
+        userExists: 'Benutzername bereits vergeben.',
+        passwortNotMatching: 'Die Passwörter stimmen nicht überein.',
+        passwortTooShort: 'Das Passwort .',
+        	passwortTooShort: 'Das Passwort .',
+        		passwortTooShort: 'Das Passwort .'
+    };
+    //endregion
+
+    //region subscribe to events
+    ideaWatcher.core.MessageBroker.subscribe(evIni);
+    //endregion
+
+    //region cbIni
+    function cbIni()
+    {
+        console.log('Initialisiere UIConnector Signup');
+
+        //region assign html elements
+        htmlFormSignup = document.querySelector('.signup_form');
+        htmlUsernameInput = document.querySelector('#signUp_userName_input');
+        htmlEmailInput = document.querySelector('#signUp_mail_input');
+        htmlPasswordInput = document.querySelector('#signUp_password_input');
+        htmlPasswordRepeatInput = document.querySelector('#signUp_passwordRepeated_input');
+        htmlSubmitButton = document.querySelector('#signUp_submit_button');
+        
+        // htmlVerificationLabel = document.querySelector('.js-login-desk-lbl-verificationError');
+        htmlView = document.querySelector('.signUp_view');
+        //endregion
+
+        //region register Callbacks
+        // wam.logic.Login.registerVerificationError(cbShowVerificationError);
+        ideaWatcher.controller.Login.registerShowView(cbShowView);
+        //endregion
+
+        // region override onSubmit to prevent page reload
+        htmlFormSignup.onsubmit = function onSubmit(event) {
+            event.preventDefault();
+        };
+        
+        htmlPasswordInput.addEventListener('change', checkValidPassword);
+        htmlPasswordRepeatInput.addEventListener('change', checkEqualPassword);
+        // endregion
+
+        //region LoginButton: Eventlistener(click)
+        htmlSubmitButton.addEventListener('click', function clickLogin() {
+
+            var exObj = {
+                userName: htmlUsernameInput.value,
+                password: htmlPasswordInput.value
+            };
+            console.log(exObj);
+
+            ideaWatcher.controller.Signup.tryToSignup(exObj);
+        });
+        //endregion
+    }
+    //endregion
+
+    function checkEqualPassword() {
+    	if (htmlPasswordInput.value == htmlPasswordRepeatInput.value)
+    		{
+    		console.log('Passwörter stimmen überein.');
+    		}
+    		
+    }
+    
+    function checkValidPassword() {
+    	if (htmlPasswordInput.value.length < 8)
+    		{
+    		console.log('Passwort ist zu kurz.');
+    		}
+    	else
+    		{
+    		console.log('Passwort ist scheinbar lang genug.');
+    		}
+    }
+    
+    //region showView
+    function cbShowView(obj)
+    {
+        if(obj.shouldShow)
+        {
+            htmlView.style.display = 'block';
+        }
+        else
+        {
+            htmlView.style.display = 'none';
+        }
+    }
+    //endregion
+
+
+    return {
+
+    };
+
+})();
