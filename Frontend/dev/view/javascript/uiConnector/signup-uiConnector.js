49837d0143c54d53c97765b00098e58b6383e5c3
Bassbiene <bassbiene@gmail.com>
2016-11-11 23:03:40 +0100

    waaaaaahhhh

--- Frontend/dev/view/javascript/uiConnector/signup-uiConnector.js
+++ Frontend/dev/view/javascript/uiConnector/signup-uiConnector.js
@@ -85,8 +85,7 @@ ideaWatcher.view.Signup = ideaWatcher.view.Signup || (function () {
     
     function checkValidPassword() {
     	
-    	var regex = new RegExp('e');
-//    			'(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\S+$).{8,}');
+    	var regex = new RegExp('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\S+$).{8,}');
     	console.log(htmlPasswordInput.value);
     	if (regex.test(htmlPasswordInput.value))
     		{
