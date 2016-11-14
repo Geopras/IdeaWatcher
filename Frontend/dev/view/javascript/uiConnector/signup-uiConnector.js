d31a76adbe1917ca474b0fbbca345cfebb05736c
LoxGITLox <sunrise122@gmx.de>
2016-11-14 12:43:18 +0100

    Die Änderungen von Bassbiene eingepflegt

--- Frontend/dev/view/javascript/uiConnector/signup-uiConnector.js
+++ Frontend/dev/view/javascript/uiConnector/signup-uiConnector.js
@@ -14,13 +14,6 @@ ideaWatcher.view.Signup = ideaWatcher.view.Signup || (function VSignup() {
     var htmlPasswordRepeatInput = null;
     var htmlSubmitButton = null;
     var htmlView = null;
-    var textSignup = {
-        userExists: 'Benutzername bereits vergeben.',
-        passwortNotMatching: 'Die Passwörter stimmen nicht überein.',
-        passwortTooShort: 'Das Passwort .',
-        	passwortTooShort: 'Das Passwort .',
-        		passwortTooShort: 'Das Passwort .'
-    };
     //endregion
 
     //region subscribe to events
@@ -46,7 +39,7 @@ ideaWatcher.view.Signup = ideaWatcher.view.Signup || (function VSignup() {
 
         //region register Callbacks
         // wam.logic.Login.registerVerificationError(cbShowVerificationError);
-        ideaWatcher.controller.Signup.registerShowView(cbShowView);
+        ideaWatcher.controller.Login.registerShowView(cbShowView);
         //endregion
 
         // region override onSubmit to prevent page reload
@@ -65,39 +58,91 @@ ideaWatcher.view.Signup = ideaWatcher.view.Signup || (function VSignup() {
 
             ideaWatcher.controller.Signup.tryToSignup(exObj);
         };
+
+        htmlPasswordRepeatInput.disabled = true;
+        htmlSubmitButton.disabled = true;
+        localizeView();
         
         // htmlPasswordInput.addEventListener('change', checkValidPassword);
         // htmlPasswordRepeatInput.addEventListener('change', checkEqualPassword);
+
+        htmlPasswordInput.addEventListener('change', checkValidPassword);
+        htmlPasswordRepeatInput.addEventListener('change', checkEqualPassword);
+
         // endregion
     }
     //endregion
 
     function checkEqualPassword() {
-    	if (htmlPasswordInput.value == htmlPasswordRepeatInput.value) {
-    		console.log('Passwörter stimmen überein.');
+        // if (htmlPasswordInput.value == htmlPasswordRepeatInput.value) {
+    		// console.log('Passwörter stimmen überein.');
+        //     return true;
+        // }
+        // else {
+        //     ideaWatcher.controller.GlobalNotification.showNotification(
+        //     'Login', 'Passwörter stimmen nicht überein!',5000);
+        //     return false
+        // }
+
+
+
+        var language = ideaWatcher.core.Localizer.getLanguage();
+        var htmlPasswordRepeatErrorLabel = document.querySelector('.signUp_passwordRepeatedError_label');
+        if (htmlPasswordInput.value == htmlPasswordRepeatInput.value) {
+            htmlPasswordRepeatErrorLabel.textContent = ideaWatcher.core.Localizer.signUp[language].passwordMatching;
+            htmlPasswordRepeatErrorLabel.style.color = 'black';
+            htmlSubmitButton.disabled = false;
             return true;
-    	}
-    	else {
-            ideaWatcher.controller.GlobalNotification.showNotification(
-            'Login', 'Passwörter stimmen nicht überein!',5000);
-            return false
+        } else {
+            htmlPasswordRepeatErrorLabel.textContent = ideaWatcher.core.Localizer.signUp[language].passwordNotMatching;
+            return false;
         }
-    		
+        htmlPasswordRepeatErrorLabel.style.display = 'inline';
+
+
+
     }
     
     function checkValidPassword() {
-    	if (htmlPasswordInput.value.length < 8)
-        {
-    		console.log('Passwort ist zu kurz.');
-            ideaWatcher.controller.GlobalNotification.showNotification(
-            'Login', 'Das Passwort ist zu kurz!',5000);
-            return false;
-    	}
-    	else
-        {
-    		console.log('Passwort ist scheinbar lang genug.');
-            return true;
-    	}
+
+        var language = ideaWatcher.core.Localizer.getLanguage();
+        var password = htmlPasswordInput.value;
+        var htmlPasswordErrorLabel = document.querySelector('.signUp_passwordError_label');
+        var isPasswordValid = true;
+        var errorMessage;
+
+        console.log(password);
+        if (password.length < 8) {
+            errorMessage = ideaWatcher.core.Localizer.signUp[language].passwordTooShort;
+            isPasswordValid = false;
+        } else if (password.search(/[a-z]/) < 0) {
+            errorMessage = ideaWatcher.core.Localizer.signUp[language].passwordMissingLowercase;
+            isPasswordValid = false;
+
+        } else if (password.search(/[A-Z]/) < 0) {
+            errorMessage = ideaWatcher.core.Localizer.signUp[language].passwordMissingUppercase;
+            isPasswordValid = false;
+        } else if (password.search(/[0-9]/) < 0) {
+            errorMessage = ideaWatcher.core.Localizer.signUp[language].passwordMissingDigit;
+            isPasswordValid = false;
+        } else if (password.search(/[{}()#:;^,.?!|&_~@$%/\=+*"'-]/) < 0) {
+            errorMessage = ideaWatcher.core.Localizer.signUp[language].passwordMissingSpecialCharacter;
+            isPasswordValid = false;
+        } else if (password.search(/[äüöß]/) > 0) {
+            errorMessage = ideaWatcher.core.Localizer.signUp[language].passwordContainsInvalidCharacter;
+            isPasswordValid = false;
+        } else {
+            console.log('Dass Passwort entspricht den Richtlinien.');
+            htmlPasswordRepeatInput.disabled = false;
+            htmlPasswordErrorLabel.style.display = 'none';
+        }
+
+        if (!isPasswordValid) {
+            htmlPasswordErrorLabel.textContent = errorMessage;
+            htmlPasswordErrorLabel.style.display = 'inline';
+        }
+
+        return isPasswordValid;
     }
     
     //region showView
@@ -111,6 +156,27 @@ ideaWatcher.view.Signup = ideaWatcher.view.Signup || (function VSignup() {
         {
             htmlView.style.display = 'none';
         }
+    }
+    //endregion
+
+    //region localizeView
+    function localizeView() {
+
+        console.log("Starte Lokalisierung der SignUp-View ...");
+
+        var language = ideaWatcher.core.Localizer.getLanguage();
+        var htmlViewHeader = document.querySelector('.signUp_header');
+        var htmlUserNameLabel = document.querySelector('#signUp_userName_label');
+        var htmlMailLabel = document.querySelector('#signUp_mail_label');
+        var htmlPasswordLabel = document.querySelector('#signUp_password_label');
+        var htmlPasswordRepeatedLabel = document.querySelector('#signUp_passwordRepeated_label');
+
+        htmlViewHeader.textContent = ideaWatcher.core.Localizer.signUp[language].header;
+        htmlUserNameLabel.textContent = ideaWatcher.core.Localizer.signUp[language].userName;
+        htmlMailLabel.textContent = ideaWatcher.core.Localizer.signUp[language].mail;
+        htmlPasswordLabel.textContent = ideaWatcher.core.Localizer.signUp[language].password;
+        htmlPasswordRepeatedLabel.textContent = ideaWatcher.core.Localizer.signUp[language].passwordRepeated;
+        htmlSubmitButton.value = ideaWatcher.core.Localizer.signUp[language].submit;
     }
     //endregion
 
