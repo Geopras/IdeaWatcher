6cbd3dd3530abf5cf041a125af3d9b1643151f8d
stefanwebo <webo@nurfuerspam.de>
2016-11-14 21:31:40 +0100

    Impressum auf deutsch eingefügt
    ProfileEditView
    - Meldungen mit Lokalisierung

--- Backend/src/main/java/de/ideaWatcher/webApi/workflow/ProfileEditWorkflow.java
+++ Backend/src/main/java/de/ideaWatcher/webApi/workflow/ProfileEditWorkflow.java
@@ -3,16 +3,142 @@ package main.java.de.ideaWatcher.webApi.workflow;
 import main.java.de.ideaWatcher.webApi.core.IRequest;
 import main.java.de.ideaWatcher.webApi.core.IResponse;
 import main.java.de.ideaWatcher.webApi.core.Response;
+import main.java.de.ideaWatcher.webApi.dataManagerInterfaces.iController.IUserController;
+import main.java.de.ideaWatcher.webApi.dataManagerInterfaces.iModel.IUser;
+import main.java.de.ideaWatcher.webApi.manager.InstanceManager;
+import org.json.JSONObject;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ProfileEditWorkflow implements IWorkflow {
 
+    private static final Logger log = Logger.getLogger( ProfileEditWorkflow.class.getName() );
+    private IUserController user;
 
-    public IResponse getResponse(IRequest data) {
+    public ProfileEditWorkflow() {
+
+        this.user = InstanceManager.getDataManager().getInstanceUser();
+    }
+
+    public IResponse getResponse(IRequest request) {
 
         // Workflow-Antwort instanziieren
         IResponse response = new Response();
 
-        response.setResult("test123");
+        // der zum Token gehörende UserName in der DB
+        String userName;
+
+        // die zum Speichern übermittelten Daten
+        String requestUsername;
+        String requestEmail;
+        String requestSurname;
+        String requestFirstName;
+        String requestGender;
+        String requestLanguage;
+        boolean requestIsMailPublic;
+        String requestPictureUrl;
+
+        try {
+            JSONObject profileData = request.getData();
+            requestUsername = profileData.getString("username");
+            requestEmail = profileData.getString("email");
+            requestSurname = profileData.getString("surname");
+            requestFirstName = profileData.getString("firstName");
+            requestGender = profileData.getString("gender");
+            requestLanguage = profileData.getString("language");
+            requestIsMailPublic = profileData.getBoolean("isMailPublic");
+            requestPictureUrl = profileData.getString("pictureUrl");
+        } catch (Exception ex) {
+            response.setErrorMessage("SProfile_saveRequestData_error");
+            response.setResult("error");
+            log.log(Level.SEVERE, "Beim Auswerten der zu Speichernden " +
+                    "Profildaten ist ein Fehler aufgetreten!" +
+                    "\nFehlermeldung: "
+                    + ex.getMessage());
+            return response;
+        }
+
+
+        //region Anhand Request-Token den Username vom TokenManager holen
+        try {
+            userName = InstanceManager.getTokenManager().getTokenValue
+                    (request.getToken());
+        } catch (Exception ex) {
+            response.setErrorMessage("SProfile_tokenNotFound_error");
+            response.setResult("error");
+            log.log(Level.SEVERE, "Bei der Zuordnung des " +
+                    "Request-Tokens zu einem Username ist " +
+                    "ein Fehler aufgetreten!\nFehlermeldung: "
+                    + ex.getMessage());
+            return response;
+        }
+        //endregion
+
+        //region Anfrage an DataManager stellen, um den zum Usernamen
+        // zugehoerigen User zu bekommen
+        IUser foundUser;
+        try {
+            foundUser = this.user.getUser(userName);
+        } catch (Exception ex) {
+            response.setErrorMessage("SProfile_getUser_error");
+            response.setResult("error");
+            log.log(Level.SEVERE, "Bei der Abfrage eines bestimmten User aus " +
+                    "der Datenbank ist ein Fehler " +
+                    "aufgetreten!\nFehlermeldung: " + ex.getMessage());
+            return response;
+        }
+        //endregion
+
+        // Prüfen, ob sich Benutzername geändert hat
+        if (!requestUsername.equals(foundUser.getUserName())){
+            // Prüfe, ob der neue UserName noch verfügbar ist
+            boolean existsUser;
+            try {
+                existsUser = this.user.existsUser(requestUsername);
+            } catch (Exception ex) {
+                response.setErrorMessage("SProfile_existsUser_error");
+                response.setResult("error");
+                log.log(Level.SEVERE, "Beim Prüfen, ob bereits ein User mit dem neuen" +
+                        " UserName existiert ist ein Fehler aufgetreten!\nFehlermeldung: " + ex.getMessage());
+                return response;
+            }
+
+            if (existsUser){
+                // Der gewählte UserName ist bereits vergeben
+                response.setErrorMessage("SProfile_userNameNotFree_error");
+                response.setResult("error");
+                return response;
+            }
+
+        }
+
+        // Pürfen, ob sich Email-Adresse geändert hat
+        if (!requestEmail.equals(foundUser.getEmail())){
+            // Prüfe, ob die neue Email-Adresse noch verfügbar ist
+            boolean existsMail;
+            try {
+                existsMail = this.user.existsEmail(requestEmail);
+            } catch (Exception ex) {
+                response.setErrorMessage("SProfile_existsEmail_error");
+                response.setResult("error");
+                log.log(Level.SEVERE, "Beim Prüfen, ob bereits ein User mit der neuen" +
+                        " Email-Adresse existiert ist ein Fehler" +
+                        " aufgetreten!\nFehlermeldung: " + ex.getMessage());
+                return response;
+            }
+
+            if (existsMail){
+                // Die gewählte Email-Adresse ist bereits vergeben
+                response.setErrorMessage("SProfile_emailNotFree_error");
+                response.setResult("error");
+                return response;
+            }
+        }
+
+        // Speichere Daten
+
+        response.setResult("success");
 
         return response;
     }
--- Frontend/dev/view/javascript/localization/profileEdit-localizer.js
+++ Frontend/dev/view/javascript/localization/profileEdit-localizer.js
@@ -12,9 +12,23 @@ ideaWatcher.core.Localizer.ProfileEdit = {
         language: 'Language',
         mail_public_available: 'allow people to contact me (Email address is' +
         ' public)',
+        saveSuccess: 'Your data has been saved successfully',
         submit: 'Submit',
         surname: 'Surname',
-        userName: 'Username'
+        userName: 'Username',
+        errorMessage: {
+            SProfile_saveRequestData_error: 'An Error occurred during the data transfer.' +
+            ' Please try again later.',
+            SProfile_tokenNotFound_error: 'Your session has expired.',
+            SProfile_getUser_error: 'An Error occurred on the server.'  +
+            ' Please try again later.',
+            SProfile_existsUser_error: 'An Error occurred on the server.'  +
+            ' Please try again later.',
+            SProfile_userNameNotFree_error: 'This UserName is already taken.',
+            SProfile_existsEmail_error: 'An Error occurred on the server.'  +
+            ' Please try again later.',
+            SProfile_emailNotFree_error: 'This email adress is already taken.'
+        }
     },
     de_DE: {
         browse: 'Durchsuchen ...',
@@ -25,8 +39,22 @@ ideaWatcher.core.Localizer.ProfileEdit = {
         language: 'Sprache',
         mail_public_available: 'Erlaube anderen mich zu kontaktieren ' +
         '(E-Mail Adresse ist öffentlich)',
+        saveSuccess: 'Ihre Daten wurden erfolgreich gespeichert',
         submit: 'Übernehmen',
         surname: 'Nachname',
-        userName: 'Benutzername'
+        userName: 'Benutzername',
+        errorMessage: {
+            SProfile_saveRequestData_error: 'Beim übermitteln der Daten ist ein Fehler aufgetreten.' +
+            ' Bitte versuchen Sie es später erneut.',
+            SProfile_tokenNotFound_error: 'Ihre Sitzung ist abgelaufen.',
+            SProfile_getUser_error: 'Auf dem Server ist ein Problem aufgetreten.' +
+            ' Bitte versuchen Sie es später erneut.',
+            SProfile_existsUser_error: 'Auf dem Server ist ein Problem aufgetreten.' +
+            ' Bitte versuchen Sie es später erneut.',
+            SProfile_userNameNotFree_error: 'Der gewünschte Benutzername ist bereits vergeben.',
+            SProfile_existsEmail_error: 'Auf dem Server ist ein Problem aufgetreten.' +
+            ' Bitte versuchen Sie es später erneut.',
+            SProfile_emailNotFree_error: 'Die gewünschte Email-Adresse ist bereits vergeben.'
+        }
     }
 }
