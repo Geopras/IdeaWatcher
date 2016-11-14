b1746ed645f8114f38b7a0762e0392ab49105b70
Bassbiene <bassbiene@gmail.com>
2016-11-14 11:31:17 +0100

    ErrorMessages in ViewLocalizer verschoben

--- Frontend/dev/view/javascript/localization/login-localizer.js
+++ Frontend/dev/view/javascript/localization/login-localizer.js
@@ -7,6 +7,8 @@ ideaWatcher.core.Localizer.login = {
         forgotPassword: "Forgot password",
         header: "Login",
         password: "Password",
+        SLogin_getUser_error: "The database query for your login has failed. Please contact the WebApi administrator.",
+    	SLogin_password_not_valid: "Login has failed.",
         submit: "Login",
         userName: "Email / Username"
     },
@@ -14,6 +16,8 @@ ideaWatcher.core.Localizer.login = {
         forgotPassword: "Passwort vergessen",
         header: "Anmeldung",
         password: "Passwort",
+        SLogin_getUser_error: "Die Anmeldung ist bei der Datenbankabfrage des Benutzers fehlgeschlagen. Bitte wenden Sie sich an den WebApi-Administrator.",
+    	SLogin_password_not_valid: "Die Anmeldung ist fehlgeschlagen.",
         submit: "Anmelden",
         userName: "Email / Benutzername"
     }
