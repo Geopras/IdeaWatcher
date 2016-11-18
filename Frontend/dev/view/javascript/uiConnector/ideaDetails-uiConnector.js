42f56701668aec2509ab9266dbdc592b6c1a7711
Bassbiene <bassbiene@gmail.com>
2016-11-18 19:55:14 +0100

    IdeaDetailsView funktioniert mit meinen Testdaten.

--- Frontend/dev/view/javascript/uiConnector/ideaDetails-uiConnector.js
+++ Frontend/dev/view/javascript/uiConnector/ideaDetails-uiConnector.js
@@ -74,12 +74,12 @@ ideaWatcher.view.IdeaDetails = ideaWatcher.view.IdeaDetails
 				localizeView();
 
 				var otherUser = ideaWatcher.model.User;
-				otherUser.username = 'x';
+				otherUser.username = 'Erika Mustermann';
 //				otherUser.pictureUrl = '';
 				
 				var comment1 = ideaWatcher.model.Comment;
 				comment1.userName = otherUser.username ;
-				comment1.pictureUrl = '';
+				comment1.pictureUrl = './resources/img/user.jpg';
 				comment1.createDate = new Date();
 				comment1.text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sagittis turpis eu eleifend finibus. Praesent non nisi tempor, imperdiet diam eu, tincidunt ex. Morbi laoreet sollicitudin faucibus. Praesent vitae velit blandit nunc posuere vehicula. Etiam sed augue quam. In hendrerit dictum nullam. ';
 				
@@ -87,7 +87,7 @@ ideaWatcher.view.IdeaDetails = ideaWatcher.view.IdeaDetails
 				user.username = 'a';
 				user.email = 'a@c.de';
 				user.isMailPublic = true;
-//				user.pictureUrl = '';
+				user.pictureUrl = './resources/img/gluehbirneLeuchtet.png';
 				
 				var ideaObject = ideaWatcher.model.Idea;
 				ideaObject.name = 'TestObjekt Idee';
@@ -169,42 +169,95 @@ ideaWatcher.view.IdeaDetails = ideaWatcher.view.IdeaDetails
 				htmlView = document.querySelector('.ideaDetails_view');
 				
 				//header
-				htmlIdeaHeader = document.createElement('h1');
-				htmlIdeaHeader.id = 'ideaDetails_ideaHeader_h1';
+				htmlIdeaHeader = document.querySelector('#ideaDetails_ideaHeader_h1');
 				htmlIdeaHeader.textContent = obj.additionalData.ideaobject.name;
 				//description
-				htmlIdeaDescription = document.createElement('div');
-				htmlIdeaDescription.id = 'ideaDetails_description_div';
+				htmlIdeaDescription = document.querySelector('#ideaDetails_description_div');
 				htmlIdeaDescription.textContent = obj.additionalData.ideaobject.description;
 				//iconbar
 					
 					//likebutton
-					htmlLikeButton = document.createElement('button');
-					htmlLikeButton.id = 'ideaDetails_like_button';
+					htmlLikeButton = document.querySelector('#ideaDetails_like_button');
+					setLikeButtonPicture(htmlLikeButton);
 					//number of likes
-					htmlLikesSpan = document.createElement('span');
+					htmlLikesSpan = document.querySelector('#ideaDetails_likes_span');
 					htmlLikesSpan.textContent = obj.additionalData.ideaobject.numberLikes;
 					
 					//starbutton
+					htmlFollowerButton = document.querySelector('#ideaDetails_follower_button');
+					setLikeButtonPicture(htmlFollowerButton);
 					//number of followers
-					//commenticon
+					htmlFollowerSpan = document.querySelector('#ideaDetails_follower_span');
+					htmlFollowerSpan.textContent = obj.additionalData.ideaobject.numberFollowers;
 					//number of comments
+					htmlCommentsSpan = document.querySelector('#ideaDetails_comments_span');
+					htmlCommentsSpan.textContent = obj.additionalData.ideaobject.numberComments;
+
 					//contactlink
-					//section
-					htmlIconSection = document.createElement('section');
-					htmlIconSection.id = 'ideaDetails_iconList_section';
-					htmlIconSection.appendChild(htmlLikeButton);
-					htmlIconSection.appendChild(htmlLikesSpan);
-				//new comment
-					//section
+					var creator = obj.additionalData.ideaobject.creator;
+					var mailaddress = creator.email;
+					var mailto = 'mailto: ' + mailaddress;
+					var htmlContactLink = document.querySelector('#ideaDetails_contact_a');
+					htmlContactLink.href = mailto;
+				//set userPicture in new Comment section
+					var htmlUserImage = document.querySelector('#ideaDetails_userPicture_img');
+					htmlUserImage.src = creator.pictureUrl;
 				//list of old comments
-					
+				htmlOldCommentsSection = document.querySelector('#ideaDetails_existingComments_section');	
+				
+				var comments = obj.additionalData.ideaobject.comments;
+				comments.forEach(function(comment){
+					//div ideaDetails_CommentNameAndText_div
+				var htmlOneCommentDiv = document.createElement('div');
+				htmlOneCommentDiv.classList.add('ideaDetails_oneComment_div');	
+						//img comment.pictureUrl
+				var htmlCommentImage = document.createElement('img');
+				htmlCommentImage.src = comment.pictureUrl;
+				htmlCommentImage.style.width = '30px';
+				htmlCommentImage.style.height = '30px';
+						//div ohne klasse
+				var htmlCommentNameAndTextDiv = document.createElement('div');
+				
+							//div ideaDetails_CommentName_div
+				var htmlCommentNameDiv = document.createElement('div');
+				htmlCommentNameDiv.classList.add('ideaDetails_CommentName_div');
+				htmlCommentNameDiv.textContent = comment.userName;
+							//div ideaDetails_CommentText_div
+				var htmlCommentTextDiv = document.createElement('div');
+				htmlCommentTextDiv.classList.add('ideaDetails_CommentText_div');
+				htmlCommentTextDiv.textContent = comment.text;
+						//append 2 divs to div
+							htmlCommentNameAndTextDiv.appendChild(htmlCommentNameDiv);
+							htmlCommentNameAndTextDiv.appendChild(htmlCommentTextDiv);
+							
+						//append img und div zu div
+							htmlOneCommentDiv.appendChild(htmlCommentImage);
+							htmlOneCommentDiv.appendChild(htmlCommentNameAndTextDiv);
+							
+				//append div zu oldOldComments
+				htmlOldCommentsSection.appendChild(htmlOneCommentDiv);
+							
+				
+				});
+				
+				//append all elements to section
 				//append all elements to view
-				htmlView.appendChild(htmlIdeaHeader);
-				htmlView.appendChild(htmlIdeaDescription);
-				htmlView.appendChild(htmlIconSection);
+				htmlView.appendChild(htmlOldCommentsSection);
+				
+				
+				htmlLikeButton.addEventListener('click', changeImageLike);
+				htmlFollowerButton.addEventListener('click',
+						changeImageFollower);
+			}
+			
+			function setLikeButtonPicture(htmlLikeButton) {
+				//wenn der Nutzer die Idee schon gelikt hat, dann zeige die leuchtende Glühbirne, ansonsten die nicht leuchtende
 			}
 
+			function setLikeButtonPicture(htmlFollowerButton) {
+				// wenn der Nutzer der Idee schon folgt, soll der leuchtende Stern angezeigt werden, ansonsten der nicht leuchtende
+			}
+			
 			return {
 
 			};
